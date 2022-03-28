471e8451373a2d571ff80ab6bd191c39b2789497
mallyar <ramnath.mallya.h@autodesk.com>
2022-03-28 21:45:34 +0530

    updated prod_sum.py file

--- prod_sum.py
+++ prod_sum.py
@@ -1,13 +1,22 @@
 
 
-number1 = 1
+num1 = int(input('give num1: '))
 
-number2 = 20 
+num2 = int(input('give num2: '))
 
-product = number1*number2
-sum = number1+number2
+def prod(number1,number2):
+    pro = number1*number2
+    return pro
+
+def sum(number1,number2):
+    su = number1+number2
+    return su
+
+product =prod(num1,num2)
+addition =sum(num1,num2)
+
 
 if product > 1000:
     print(product)
 else:
-    print(sum) 
+    print(addition) 
